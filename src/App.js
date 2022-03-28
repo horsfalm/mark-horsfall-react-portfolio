6e511832df8b0a7be1ca2053fafb350f9b6483ed
BTV <horsfalm@gmail.com>
2022-03-28 12:04:05 +1100

    add contact porfolio and resume components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
-import About from './components/About';
+import About from './components/about';
+import Contact from './components/contact';
+import Portfolio from './components/portfolio';
+import Resume from './components/about';
+
 
 function App() {
   return (
